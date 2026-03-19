ed6995068c19f3c280f9ba4482c72b93f273373f
KvotheGuile <kvothejoestar@gmail.com>
2026-03-19 14:15:38 -0700

    feat: database user and password moved to .env file

--- back-end/src/connection/connection.ts
+++ back-end/src/connection/connection.ts
@@ -3,11 +3,19 @@ import { Company } from "../models/company";
 import { User } from "../models/user";
 import { Location } from "../models/location";
 
+const { loadEnvFile } = require('node:process');
+loadEnvFile('.env');
+
+const userID = process.env.USERNAME_SQL ?? "unknown";
+const userPassword = process.env.PASSWORD_SQL ?? "unknown";
+
+console.log(`username: ${userID}, password: ${userPassword}`);
+
 const connection = new Sequelize({ 
     database: 'reto_db_final', 
     dialect: 'postgres',
-    username: 'reto_admin', 
-    password: 'prueba123', 
+    username: userID, 
+    password: userPassword, 
     models: [
         Company,
         User,
