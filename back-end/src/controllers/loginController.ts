e4cd45ed96ea35eb249a220f3bbf43027acae4ac
KvotheGuile <kvothejoestar@gmail.com>
2026-03-24 13:50:54 -0700

    feat: login API now returns a JWT in the payload

--- back-end/src/controllers/loginController.ts
+++ back-end/src/controllers/loginController.ts
@@ -1,6 +1,7 @@
 import { RequestHandler, Request, Response } from "express";
 import { User } from "../models/user";
 import { verifyPassword } from "../security/hashing";
+import { createToken } from "../security/jwt";
 
 export const loginAuthentication: RequestHandler = async (req: Request, res: Response) => {
 
@@ -17,13 +18,17 @@ export const loginAuthentication: RequestHandler = async (req: Request, res: Res
         verifyPassword(req.body.password, hashedPassword).then(
             result => {
                 if (result){  
+                    
+                    const jwt = createToken(user);
+
                     return res.status(200).json({
                         status: "success",
                         message: "User verified",
-                        payload: user.toJSON(),
+                        payload: jwt,
                     });
                 }else{
                     return res.status(401).json({
+                        status: "fail",
                         message: "Error. Incorrect email or password"
                     })
                 }
