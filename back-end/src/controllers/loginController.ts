827154774a64a9ddb289253e15a6637a61219c91
KvotheGuile <kvothejoestar@gmail.com>
2026-03-24 13:25:38 -0700

    feat: api for verifying user credentials working

--- back-end/src/controllers/loginController.ts
+++ back-end/src/controllers/loginController.ts
@@ -5,5 +5,34 @@ import { verifyPassword } from "../security/hashing";
 export const loginAuthentication: RequestHandler = async (req: Request, res: Response) => {
 
     console.log(req.body);
-    return res.status(200).json(req.body); // Test function
+    
+    const user = await User.findOne({
+        where: {
+            email: req.body.email
+        }
+    });
+
+    if (user) {
+        const hashedPassword = user.password;
+        verifyPassword(req.body.password, hashedPassword).then(
+            result => {
+                if (result){  
+                    return res.status(200).json({
+                        status: "success",
+                        message: "User verified",
+                        payload: user.toJSON(),
+                    });
+                }else{
+                    return res.status(401).json({
+                        message: "Error. Incorrect email or password"
+                    })
+                }
+            }
+        );
+
+    } else {
+        return res.status(401).json({
+            message: "Error. User not found",
+        });
+    }
 };
--- back-end/src/security/hashing.ts
+++ back-end/src/security/hashing.ts
@@ -6,3 +6,9 @@ export async function hashPassword (password: string)
     const result = await bcrypt.hash(password, saltRounds);
     return result;
 }
+
+export async function verifyPassword(password: string, hash: string)
+{
+    const match = await bcrypt.compare(password, hash);
+    return match;
+}
