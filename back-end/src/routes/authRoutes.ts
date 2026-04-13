87d0f37df634ca6c264c6e50d708e8fbd3547be9
KvotheGuile <kvothejoestar@gmail.com>
2026-04-12 19:12:55 -0700

    refactor: loginController renamed to authMiddleware

--- back-end/src/routes/authRoutes.ts
+++ back-end/src/routes/authRoutes.ts
@@ -1,6 +1,6 @@
 
 import { Router } from 'express';
-import { loginAuthentication } from '../middlewares/loginController';
+import { loginAuthentication } from '../middlewares/authMiddleware';
 
 const authRouter: Router = Router();
 
