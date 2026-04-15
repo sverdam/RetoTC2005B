4cfb950932485c3794fac54c2e3ef70040ab58fc
snwflwrsoph <asophia.peraltar@gmail.com>
2026-04-14 19:52:35 -0700

    Feat: Add types to frontend

---
+++ front-end/src/types/types.d.ts
@@ -0,0 +1,85 @@
+declare module "clas-types" {
+    export interface Category {
+        id: number;
+        name: string;
+    }
+
+    export interface Company {
+        id: number;
+        name: string;
+        description: string;
+        tier: number;
+        // logo: Blob;
+    }
+
+    export enum ContactType {
+        EMAIL = 'email',
+        PHONE = 'phone',
+    }
+
+    export interface Contact {
+        id: number; 
+        type: ContactType; 
+        contactInfo: string;
+        companyId: number; 
+    }
+
+    export enum FileType {
+        LOGO = 'logo',
+        IMAGE = 'image',
+        DOCUMENT = 'document'
+    }
+
+    export interface FileModule{
+        id: number;
+        companyId: number;
+        position: number;
+        type: FileType;
+        storedName: string | null;
+        originalName: string | null;
+        path: string | null;
+        mimeType: string | null;
+        size: number | null;
+    }
+
+    interface Filter{ 
+        id: number; 
+        name: string; 
+        tier: number;
+        categoryId: number;
+    } 
+    
+    interface Location{ 
+        id: number; 
+        name: string; 
+        street: string; 
+        number: number;
+        neighborhood: string;
+        postalCode: string;
+        city: string; 
+        country: string;
+        companyId: number;
+    }
+
+    interface TextModule{ 
+        id: number; 
+        title: string; 
+        text: Text; 
+        tier: number;
+        companyId: number;
+    } 
+
+    enum UserRole {
+        ADMIN = 'admin',
+        USER = 'user',
+    }
+
+    interface User{ 
+        id: number; 
+        name: string; 
+        email: string; 
+        password: string; 
+        role: UserRole;
+        companyId: number;
+    } 
+}
