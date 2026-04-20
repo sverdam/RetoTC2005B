ec763498e66b979f2b2f8f421c8c337d70db90b4
snwflwrsoph <asophia.peraltar@gmail.com>
2026-04-19 19:10:37 -0700

    Fix: Directory Card

--- front-end/src/components/DirectoryCard.tsx
+++ front-end/src/components/DirectoryCard.tsx
@@ -1,5 +1,5 @@
 import type { Company } from "clas-types";
-import { InformationCircleIcon, StarIcon } from "@heroicons/react/24/outline";
+import { InformationCircleIcon } from "@heroicons/react/24/outline";
 
 interface Props {
     company: Company;
@@ -12,21 +12,20 @@ const DirectoryCard: React.FC<Props> = ({ company }) => {
         <div className="p-2 border border-gray-200 rounded-lg flex flex-col gap-3 items-start">
             {/* Logo */}
             <img 
-                src={!company?.logo ? "..\assets\Default_Logo.jpeg" : String(company.logo.path)} // TODO: Arreglar esto!!
+                src={!company?.logo ? "../src/assets/logoipsum.png" : String(company.logo.path)} // TODO: Arreglar esto!!
                 alt={company.name}
                 className="w-full h-25 object-contain"
             />
             <div className="flex gap-2 items-center">
                 <InformationCircleIcon className="h-5 w-5 text-gray-400"/>
                 <h4 className="text-gray-400">{company.memberType}</h4>
-                <StarIcon className="h-5 w-5 text-gray-400"/>
                 <h4 className="text-gray-400">|</h4>
-                <h4 className="text-gray-400">{company.tier}</h4>
+                <h4 className="text-gray-400">Tier {company.tier}</h4>
             </div>
-            <h2 className="text-lg font-medium">
+            <h2 className="text-lg font-medium text-left">
                 {company.name}
             </h2>
-            <p className="text-gray-400">{!company.location ? "No existe dirección ingresada aún..." :company.location.address }</p>
+            <p className="text-gray-400 text-left">{!company.location ? "No existe dirección ingresada aún..." :company.location.address }</p>
 
             <button className="font-medium text-sm text-gray-400">
                 Leer más
