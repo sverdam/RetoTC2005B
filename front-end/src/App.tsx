e1a4ea328ce11fd1d45d472615cf55801fdd5831
Sofía Verdugo <sverdugoam@gmail.com>
2026-04-20 23:03:56 -0700

    feat: add navbar component to app

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,13 +1,14 @@
 import type { FC } from 'react'
 import { Outlet } from 'react-router'
 import './App.css'
-import Footer from './components/Footer';
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 
 const App: FC = () => {
   return (
     <div className="min-h-screen bg-gray-50">
       <header>
-        {/* TODO: NAVBAR */}
+        <Navbar/>
       </header>
       <main className="max-w-6xl mx-auto px-4 py-6">
         <Outlet />
