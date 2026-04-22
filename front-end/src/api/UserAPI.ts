d477df963bcc51aa52d7b728fd2f510aa20df9df
KvotheGuile <kvothejoestar@gmail.com>
2026-04-22 14:56:49 -0700

    feat: restore default updateUser endpoint

--- front-end/src/api/UserAPI.ts
+++ front-end/src/api/UserAPI.ts
@@ -57,7 +57,7 @@ export const createUser = async (data: NewUserInput): Promise<User> => {
 
 export const updateUser = async (id: number, data: NewUserInput): Promise<User> => {
     try {
-        const res = await api.patch<User>(`/user/password/${id}`, data);
+        const res = await api.patch<User>(`/user/${id}`, data);
 
         return res.data;
     } catch (error){
