f617af7b6028138feed96f50559ee7f33034a808
snwflwrsoph <asophia.peraltar@gmail.com>
2026-04-21 23:59:46 -0700

    Feat: Add File Upload Component

---
+++ front-end/src/components/FileUpload.tsx
@@ -0,0 +1,50 @@
+// sophhh
+import {useState} from "react";
+import { DocumentArrowUpIcon } from "@heroicons/react/24/outline";
+
+interface Props {
+    onFileSelect: (file: File) => void;
+};
+
+const FileUpload: React.FC<Props> = ({ onFileSelect }) => {
+
+    const [fileName, setFileName] = useState< string | null >(null);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (file) {
+            setFileName(file.name);
+            onFileSelect(file);
+        }
+    };
+
+    return(
+        <div className="w-md">
+            <label
+                htmlFor="fileUpload"
+                className="flex flex-col items-center justify-center w-md h-44 border-2 border-dashed border-clas-gris rounded-lg cursor-pointer hover:bg-clas-gris/20 transition"
+            >
+                <input 
+                    id="fileUpload"
+                    type="file"
+                    className="hidden"
+                    onChange={handleChange}
+                />
+                <div className="flex flex-col items-center gap-2">
+                    <div>
+                        <DocumentArrowUpIcon className="h-7 w-7 text-clas-gris"/>
+                    </div>
+                    <span className="text-clas-negro font-medium">
+                        {fileName ? fileName : "Sube tu archivo"}
+                    </span>
+                    <span className="text-sm text-clas-negro">
+                        Haz click para seleccionar
+                    </span>
+                </div>
+
+            </label>
+        </div>
+    )
+}   
+
+export default FileUpload;
