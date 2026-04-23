7fd972ee081a066974554f21d726c09e9173fd18
KvotheGuile <kvothejoestar@gmail.com>
2026-04-22 22:42:09 -0700

    feat: credentials added by default with axios

--- front-end/src/api/index.ts
+++ front-end/src/api/index.ts
@@ -2,6 +2,7 @@ import axios, {type AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
     baseURL: "http://localhost:3000/",//TODO: Agregar url de backend
+    withCredentials: true,
     headers: {
         "Content-Type": "application/json",
     },
