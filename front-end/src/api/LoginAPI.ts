22e2c604dc5049f4d16df65bd62ec0bfd94cba51
KvotheGuile <kvothejoestar@gmail.com>
2026-04-23 00:07:31 -0700

    feat: loginAPI getProfile added

--- front-end/src/api/LoginAPI.ts
+++ front-end/src/api/LoginAPI.ts
@@ -15,3 +15,17 @@ export const login = async (data: LoginUser) : Promise <any> => {
     throw err;
   }
 }
+
+export const getProfile = async() : Promise<any> => {
+    try {
+        const res = await api.get<any>("/auth/profile");
+        console.log(res.data.payload);
+        return res.data.payload;
+  } catch (error) {
+    const err = error as AxiosError;
+
+    console.error("Error creating user:", err.message);
+
+    throw err;
+  }
+}
