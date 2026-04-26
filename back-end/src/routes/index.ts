72f518086644bc7c42037bada354177ee0a0ccc9
KvotheGuile <kvothejoestar@gmail.com>
2026-04-25 23:40:13 -0700

    fix: merge conflict solution

--- back-end/src/routes/index.ts
+++ back-end/src/routes/index.ts
@@ -8,11 +8,8 @@ import filterRoutes from './filterRoutes';
 import textModuleRoutes from './textModuleRoutes';
 import authRoutes from './authRoutes';
 import fileModuleRoutes from './fileModuleRoutes';
-<<<<<<< HEAD
 import productRoutes from './productRoutes';
-=======
 import serviceRouter from './serviceRoutes';
->>>>>>> 0cb9a3a2f5ce6110c578551ddc4495334377158b
 import certificationRouter from './certificationRoutes';
 
 const apiRouter:Router = Router();  
@@ -27,13 +24,9 @@ apiRouter.use('/category', categoryRoutes);
 apiRouter.use('/filter', filterRoutes);
 apiRouter.use('/textModule', textModuleRoutes);
 apiRouter.use('/fileModule', fileModuleRoutes);
-<<<<<<< HEAD
 apiRouter.use('/product', productRoutes);
-apiRouter.use('/certifications', certificationRouter);
-=======
 apiRouter.use('/service', serviceRouter)
 apiRouter.use('/certification', certificationRouter);
->>>>>>> 0cb9a3a2f5ce6110c578551ddc4495334377158b
 
 apiRouter.get('/', (req:Request, res: Response) => {  
 res.send('Hello World!')  
