9468855e2dfc6367db8bdc9db4f84ba41e41e27a
snwflwrsoph <asophia.peraltar@gmail.com>
2026-04-27 23:42:04 -0700

    Style: New Company Button Size

--- front-end/src/components/NewDirectoryCardButton.tsx
+++ front-end/src/components/NewDirectoryCardButton.tsx
@@ -5,7 +5,7 @@ const NewDirectoryCardButton: React.FC = () => {
 	const navigate = useNavigate();
 	
 	return(
-		<div className="border-2 border-clas rounded-lg flex flex-col gap-3 items-start hover:bg-clas/20"
+		<div className="min-w-50 min-h-50 border-2 border-clas rounded-lg flex flex-col gap-3 items-start hover:bg-clas/20"
 		>
 			<button className="w-full h-full flex justify-center items-center hover:bg-clas/20"
 				onClick={() => navigate("/empresa/editar")}
