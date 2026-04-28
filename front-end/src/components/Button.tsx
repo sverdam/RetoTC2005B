37859ac3ccca21606b0f1088a83b789cfcc4b7d8
Lul3n <1lupitafierrosvzla@gmail.com>
2026-04-27 20:16:53 -0700

    feat connection to editPage

--- front-end/src/components/Button.tsx
+++ front-end/src/components/Button.tsx
@@ -1,17 +1,21 @@
+import type { Company } from "clas-types";
 import { NavLink } from "react-router-dom";
 
 interface Props{
     text: string;
     to: string;
+    comp?: Company;
 }
 
-const Button: React.FC<Props> = ({text, to}) => {
+const Button: React.FC<Props> = ({text, to, comp}) => {
     return (
     <div className="flex items-center">
         <NavLink
             to={to}
+            state={comp}
             className="inline-flex items-center justify-end text-white bg-clas hover:bg-clas-claro focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none focus:ring-offset-2"
             >
+            
             {text}
             </NavLink>
             </div>
