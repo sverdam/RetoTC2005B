4577711c9b4e391e7e2a2b89a3d276ff0f8cf868
KvotheGuile <kvothejoestar@gmail.com>
2026-04-30 02:34:11 -0700

    test: remove unused debug

--- front-end/src/api/fileModuleAPI.ts
+++ front-end/src/api/fileModuleAPI.ts
@@ -4,7 +4,7 @@ import { AxiosError } from "axios";
 
 export const getFileURLById = (id: number) => {
     const url = `${api.defaults.baseURL}fileModule/files/${id}`;
-    console.log(`URL: ${url}`);
+    //console.log(`URL: ${url}`);
     return url;
 }
 
