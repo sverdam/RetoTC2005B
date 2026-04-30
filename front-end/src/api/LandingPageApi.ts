faf26da81567d8e8b50366081d437d2c5ffd02fb
Lul3n <1lupitafierrosvzla@gmail.com>
2026-04-30 03:52:35 -0700

    refactor indentation of api and quit console.log

--- front-end/src/api/LandingPageApi.ts
+++ front-end/src/api/LandingPageApi.ts
@@ -5,7 +5,7 @@ import type { LandingPage } from 'clas-types'
 export const getLandingPage = async (): Promise<LandingPage> => {
     try {
         const res = await api.get<LandingPage>(`/landing`);
-        console.log(res.data);
+        //sconsole.log(res.data);
         return res.data;
     } catch (error) {
         const err = error as AxiosError;
